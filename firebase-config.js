b37d1717c41a725a863a383155b448c6eae094a5
nuttapol7610-create <nuttapol7610@gmail.com>
2026-07-20 00:05:57 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,15 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDI3vFkce2cRLBYC0isE1Yj_t3rV4YSYWw",
-  authDomain: "task-manager-c3ab5.firebaseapp.com",
-  projectId: "task-manager-c3ab5",
-  storageBucket: "task-manager-c3ab5.firebasestorage.app",
-  messagingSenderId: "160284229956",
-  appId: "1:160284229956:web:bc488c85fdc4f380a29711",
-  measurementId: "G-VZYN4H033P"
+export const firebaseConfig = {
+2
+apiKey: "AIzaSyDI3vFkce2cRLBYC0isE1Yj_t3rV4YSYWw",
+3
+authDomain: "task-manager-c3ab5.firebaseapp.com",
+4
+projectId: "task-manager-c3ab5",
+5
+storageBucket: "task-manager-c3ab5.firebasestorage.app",
+6
+messagingSenderId: "160284229956",
+7
+appId: "1:160284229956:web:bc488c85fdc4f380a29711"
+8
 };
